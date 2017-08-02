f906958c5b85609d7c1f1773589c900b6563fcde
Jackson Chen <jackson.chen@columbia.edu>
2017-08-01 20:27:03 -0400

    Message passing from content scipt up works

--- popup.js
+++ popup.js
@@ -22,13 +22,21 @@ document.addEventListener('DOMContentLoaded', function() {
       renderElem('The following error occurred: ' + err, 'status');
     }
     else {
-      chrome.tabs.update({ url: 'https://www.facebook.com/search/people/?q=my%20friends%20who%20go%20to%20university%20of%20colorado%20boulder' }, function() {
-          // chrome.tabs.executeScript(null, { file: 'jquery-3.2.1.min.js' }, function() {
-          //   chrome.tabs.executeScript(null, { file: 'scroller.js' }, function() {
-          //     renderElem('Here is an analysis of your friends:', 'status');
-          //   });
-          // });
+      chrome.tabs.update({ url: 'https://www.facebook.com/search/83086245170/students/present/me/friends/intersect' }, function() {
+          chrome.tabs.executeScript(null, { file: 'jquery-3.2.1.min.js' }, function() {
+            chrome.tabs.executeScript(null, { file: 'scroller.js' }, function() {
+              renderElem('Here is an analysis of your friends:', 'status');
+            });
+          });
       });
+
+      chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+          if (request.msg === "found_friends") {
+            console.log(request.data);
+          }
+        }
+      );
     }
   });
 });
--- scroller.js
+++ scroller.js
@@ -1,1 +1,23 @@
-alert('hi');
+var i = 0;
+var friends = [];
+
+$(document).ready(function() {
+  console.log(school);
+  scrollDown();
+});
+
+function scrollDown() {
+  setTimeout(function () {
+    window.scrollTo(0,document.body.scrollHeight);
+    i++;
+    if ($('#browse_end_of_results_footer').length == 0 && i < 100) {
+      scrollDown();
+    }
+    else {
+      friends = $.map($('._4e2r').find('._32mo'), function(val) {
+          return val.innerText;
+      });
+      chrome.runtime.sendMessage({msg: "found_friends", school: "CU Boulder", data: friends});
+    }
+  }, 1000);
+}
